2c1b5dadd50c272447b6938f04f3fd9b794f1712
zmzlois <lois.z@comcord.vision>
2025-12-11 00:07:54 +0000

    fix: error handling during exit on native module exception

--- src/bun.js/bindings/ZigGlobalObject.cpp
+++ src/bun.js/bindings/ZigGlobalObject.cpp
@@ -266,6 +266,8 @@ extern "C" unsigned getJSCBytecodeCacheVersion()
 extern "C" void Bun__REPRL__registerFuzzilliFunctions(Zig::GlobalObject*);
 #endif
 
+extern "C" int32_t bun_is_exiting();
+
 extern "C" void JSCInitialize(const char* envp[], size_t envc, void (*onCrash)(const char* ptr, size_t length), bool evalMode)
 {
     static std::once_flag jsc_init_flag;
@@ -273,7 +275,23 @@ extern "C" void JSCInitialize(const char* envp[], size_t envc, void (*onCrash)(c
     std::call_once(jsc_init_flag, [evalMode, envp, envc, onCrash]() {
         JSC::Config::enableRestrictedOptions();
 
-        std::set_terminate([]() { Zig__GlobalObject__onCrash(); });
+        std::set_terminate([]() {
+            // if bun is already exiting, a native module threw an exception
+            // during cleanup (e.g. in a destructor). don't crash - just exit.
+            if (bun_is_exiting()) {
+                fprintf(stderr,
+                    "oh no: A native module threw a C++ exception during process exit.\n"
+                    "\n"
+                    "This is likely a bug in the native module, not Bun.\n"
+                    "If you believe this is a Bun issue or Bun can help fix it, please file a bug report:\n"
+                    "\n"
+                    "  https://github.com/oven-sh/bun/issues\n"
+                    "\n");
+                WTFReportBacktrace();
+                _Exit(1);
+            }
+            Zig__GlobalObject__onCrash();
+        });
         WTF::initializeMainThread();
 
 #if ASAN_ENABLED && OS(LINUX)
